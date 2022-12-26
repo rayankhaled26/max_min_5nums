8329bae08f48ee835a4628e9351ff7d1ce1d1070
rayankhaled26 <rk217230@gmail.com>
2022-12-26 15:46:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+num1 = input("Enter number: ")
+num2 = input("Enter number: ")
+num3 = input("Enter number: ")
+num4 = input("Enter number: ")
+num5 = input("Enter number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+num4 = int(num4)
+num5 = int(num5)
+
+nums = [num1, num2, num3, num4, num5]
+
+print(max(nums), "is maximum")
+print(min(nums), "is minimum")
